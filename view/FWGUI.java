25fbbb3fa388737705e2a980e3e061dbaca29e45
Manjinder Ghuman <msghuman02@gmail.com>
2025-02-03 16:23:12 -0800

    More GUI

--- view/FWGUI.java
+++ view/FWGUI.java
@@ -1,20 +1,11 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import javax.swing.*;
 
 public class FWGUI implements ActionListener {
-
     private JFrame myFrame;
     private JMenuBar myMenuBar;
     private int runningTime = 0;
@@ -27,16 +18,12 @@ public class FWGUI implements ActionListener {
      * Constructor for the GUI. This will create the GUI and set up the menu bar.
      */
     public FWGUI() {
-        super();
         myFrame = new FWFrame().frameOutline();
-        // Create the menu bar and start the timer when necessary.
+        myFrame.setLayout(new BorderLayout());
         createMenuBar();
         timeKeeper();
-        // Create a panel for the time label
-        JPanel timePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        timePanel.add(myTimeLabel);
-        // Add the time panel to the frame.
-        myFrame.add(timePanel, BorderLayout.SOUTH);
+        JPanel mainPanel = new FWPanel();
+        myFrame.add(mainPanel, BorderLayout.CENTER);
         myFrame.setVisible(true);
     }
 
@@ -44,12 +31,10 @@ public class FWGUI implements ActionListener {
      * This method will keep track of the time that the user has been monitoring files.
      */
     private void timeKeeper() {
-        // Create a timer to keep track of time (VSCode is extremely helpful.)
         myTimer = new Timer(1000, (ActionEvent e) -> {
             runningTime++;
             timerLabelExtended();
         });
-        // Add action listeners to the start and stop buttons.
         myStartButton.addActionListener(this);
         myStopButton.addActionListener(this);
     }
@@ -58,58 +43,38 @@ public class FWGUI implements ActionListener {
      * This method will create the menu bar for the GUI.
      */
     private void createMenuBar() {
-        // Create the menu bar
         myMenuBar = new JMenuBar();
-
-        // Create menus
         JMenu fileMenu = new JMenu("File");
         JMenu watcherMenu = new JMenu("File System Watcher");
         JMenu databaseMenu = new JMenu("Database");
         JMenu aboutMenu = new JMenu("About");
-
-        // Create a label for the time.
         myTimeLabel = new JLabel("Time not started.");
-
-        // Add menu items for "File"
         myStartButton = new JMenuItem("Start");
         myStopButton = new JMenuItem("Stop");
         JMenuItem queryItem = new JMenuItem("Query Database(file extension)");
         JMenuItem closeItem = new JMenuItem("Close");
-        myStartButton.setEnabled(true); // Disable startItem by default
-        myStopButton.setEnabled(false); // Disable stopItem by default
+        myStartButton.setEnabled(true);
+        myStopButton.setEnabled(false);
         fileMenu.add(myStartButton);
         fileMenu.add(myStopButton);
         fileMenu.add(queryItem);
         fileMenu.add(closeItem);
-
-        // Add action listener to closeItem
         closeItem.addActionListener(this);
-
-        // Add menu items to "File System Watcher"
         JMenuItem startWatcherItem = new JMenuItem("Start Watching");
         JMenuItem stopWatcherItem = new JMenuItem("Stop Watching");
         watcherMenu.add(startWatcherItem);
         watcherMenu.add(stopWatcherItem);
-
-        // Add menu items to "Database"
         JMenuItem connectDbItem = new JMenuItem("Connect to Database");
         JMenuItem disconnectDbItem = new JMenuItem("Disconnect Database");
         databaseMenu.add(connectDbItem);
         databaseMenu.add(disconnectDbItem);
-
-        // Add Help menu
         JMenuItem aboutHelpItem = new JMenuItem("About");
-        // Add action listener to aboutHelpItem.
         aboutHelpItem.addActionListener(this);
         aboutMenu.add(aboutHelpItem);
-
-        // Add menus to the menu bar
         myMenuBar.add(fileMenu);
         myMenuBar.add(watcherMenu);
         myMenuBar.add(databaseMenu);
         myMenuBar.add(aboutMenu);
-
-        // Attach menu bar to the frame
         myFrame.setJMenuBar(myMenuBar);
     }
 
@@ -143,13 +108,12 @@ public class FWGUI implements ActionListener {
     /*
      * This method will extend the timer label to show the time in days, hours, minutes, and seconds.
      */
-    private void timerLabelExtended(){
+    private void timerLabelExtended() {
         int days = runningTime / 86400;
         int hours = (runningTime % 86400) / 3600;
         int minutes = (runningTime % 3600) / 60;
         int seconds = runningTime % 60;
-
-        String timeFormatted = String.format("Time Running: %02d Days: %02d Hours: %02d Minutes: %02d Seconds",days,hours,minutes,seconds);
+        String timeFormatted = String.format("Time Running: %02d Days: %02d Hours: %02d Minutes: %02d Seconds", days, hours, minutes, seconds);
         myTimeLabel.setText(timeFormatted);
     }
 }
